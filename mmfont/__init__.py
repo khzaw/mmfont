b93b82062c03120ca40e773e4982e82f6faf3be7
Kaung Htet Zaw <emoosx@gmail.com>
2015-02-27 00:40:56 +0800

    Initial commit

---
+++ mmfont/__init__.py
@@ -0,0 +1,2 @@
+__all__ = ['mmfont']
+import mmfont
---
+++ setup.py
@@ -0,0 +1,11 @@
+from disutils.core import setup
+setup(
+    name = 'mmfont',
+    packages = ['mmfont'],
+    version = '0.1',
+    description = 'Myanmar Font Encoding Converter'
+    author = 'Kaung Htet Zaw',
+    author_email = 'emoosx@gmail.com',
+    url = 'https://github.com/emoosx/mmfont',
+    keywords = ['myanmar', 'unicode', 'mm', 'zawgyi', 'mmunicode']
+)
