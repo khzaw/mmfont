a4fe9dbd4177463c88f56913d56bd1b9ef3ea9f8
Kaung Htet Zaw <emoosx@gmail.com>
2015-02-27 03:27:28 +0800

    Add download_url

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ setup(
     name = 'mmfont',
     packages = ['mmfont'],
     version = '0.1',
-    description = 'Myanmar Font Encoding Converter'
+    description = 'Myanmar Font Encoding Converter between Zawgyi 1 and Unicode 5.1 encodings',
     author = 'Kaung Htet Zaw',
     author_email = 'emoosx@gmail.com',
     url = 'https://github.com/emoosx/mmfont',
+    download_url = 'https://github.com/emoosx/mmfont/tarball/0.1',
     keywords = ['myanmar', 'unicode', 'mm', 'zawgyi', 'mmunicode']
 )
